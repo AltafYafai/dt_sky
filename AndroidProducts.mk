b875f5b42a013e289816268df529c9a810f58fda
TopexGuy <topexguy@gmail.com>
2026-01-26 10:09:46 +0530

    sky: update: multi ROMs Bringups

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -8,6 +8,7 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/infinity_sky.mk \
     $(LOCAL_DIR)/voltage_sky.mk \
     $(LOCAL_DIR)/lineage_sky.mk \
+    $(LOCAL_DIR)/custom_sky.mk \
     $(LOCAL_DIR)/euclid_sky.mk \
     $(LOCAL_DIR)/clover_sky.mk \
     $(LOCAL_DIR)/alpha_sky.mk
